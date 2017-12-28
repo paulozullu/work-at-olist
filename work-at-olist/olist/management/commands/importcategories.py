b22a7d3537f47070428517de674ec6d959ed788d
Paulo Santos <paulozullu@gmail.com>
2017-12-28 00:37:48 -0300

    Moved importategories.py

---
+++ work-at-olist/olist/management/commands/importcategories.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from django.core.management.base import BaseCommand, CommandError
+from olist.models import Category, Channel
+
+class Command(BaseCommand):
+    help = "Imports categories"
+
+    def add_arguments(self, parser):
+        parser.add_argument('arguments', nargs='+', type=str)
+
+
+    def handle(self, *args, **options):
+        if len(options['arguments']) == 2:
+            channel_name = options['arguments'][0]
+            filename = options['arguments'][1]
+    
+
+    def import_categories(self, channel_name, filename):
+        """
+        Import categories to the database.
+
+            Args:
+                channel_name (str): The name of the channel that owns the categories.
+                filename (str): Path to the file containing the categories to be imported.
+        """
+        
+        channel, created = Channel.objects.get_or_create(name=channel_name)
+        
+        file = open(filename, 'r').readlines()
+        
+        for row in file:
+            categories = row.split('/')
+            depth = len(categories) - 1
+            parent_id = None
+
+            for x in range(0, depth):
+                category, created = Category.objects.get_or_create(name=category[x].trim(), channel_id=result.id, parent_id=parent_id)
+                parent_id = category.id
+
+
+               
+
+
--- work-at-olist/olist/models.py
+++ work-at-olist/olist/models.py
@@ -4,10 +4,27 @@ from django.db import models
 
 class Category(models.Model):
     """
-    Categories, channels and subcategories. Uses adjacency model.
+    Categories and subcategories. Uses adjacency model.
     """
     name = models.CharField(max_length=50,blank=True,null=False)
     parent_id = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    channel_id = models.ForeignKey('Channel', on_delete=models.CASCADE, null=False, blank=False)
+
+    class Meta:
+        db_table = 'category'
+    
+    def __unicode__(self):
+        return self.name
+    
+
+class Channel(models.Model):
+    """
+    Channels.
+    """
+    name = models.CharField(max_length=50,blank=False,null=False)
+
+    class Meta:
+        db_table = 'channel'
 
     def __unicode__(self):
         return self.name

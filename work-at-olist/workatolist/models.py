9efafdd9cecd5d7b58de9222caa3c3876ddfeb8e
Paulo Santos <paulozullu@gmail.com>
2017-12-27 23:24:23 -0300

    Allow empty name for the categories

--- work-at-olist/workatolist/models.py
+++ work-at-olist/workatolist/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
     """
     Categories, channels and subcategories. Uses adjacency model.
     """
-    name = models.CharField(max_length=50,blank=False,null=False)
+    name = models.CharField(max_length=50,blank=True,null=False)
     parent_id = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
 
     def __unicode__(self):

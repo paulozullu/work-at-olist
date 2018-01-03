f9719beebde2f00125161f35fc98b7e97cc80826
Paulo Santos <paulozullu@gmail.com>
2018-01-02 22:20:32 -0300

    Added prints on categories importation.

--- work-at-olist/olist/management/commands/importcategories.py
+++ work-at-olist/olist/management/commands/importcategories.py
@@ -25,8 +25,13 @@ class Command(BaseCommand):
                 filename (str): Path to the file containing the categories to be imported.
         """
         
-        channel, _ = Channel.objects.get_or_create(name=channel_name)
+        channel, channel_created = Channel.objects.get_or_create(name=channel_name)
         
+        if channel_created:
+            print("Channel {0} created.".format(channel_name))
+        else:
+            print("Channel {0} updated.".format(channel_name))
+
         file = open(filename, 'r').readlines()
         
         for row in file:
@@ -34,6 +39,17 @@ class Command(BaseCommand):
             parent = None
 
             for category in categories:
-                category_obj, created = Category.objects.get_or_create(name=category.strip(), channel=channel, parent=parent)
+                category_obj, category_created = Category.objects.get_or_create(name=category.strip(), channel=channel, parent=parent)
+
+                if category_created:
+                    if parent is None:
+                        print("Category {0} created.".format(category.strip()))
+                    else: 
+                        print("Category {0}->{1} created.".format(parent.name, category.strip()))
+                else:
+                    if parent is None:
+                        print("Category {0} updated.".format(category.strip()))
+                    else: print("Category {0}->{1} updated.".format(parent.name, category.strip()))
+
                 parent = category_obj
         

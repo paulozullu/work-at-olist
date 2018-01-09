bc1f0d860a2199d117c083d536d568ff0473f597
Paulo Santos <paulozullu@gmail.com>
2018-01-09 19:07:29 -0300

    Fix wsgi

--- work-at-olist/workatolist/wsgi.py
+++ work-at-olist/workatolist/wsgi.py
@@ -15,4 +15,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "workatolist.settings")
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/static/')
+application = WhiteNoise(application)

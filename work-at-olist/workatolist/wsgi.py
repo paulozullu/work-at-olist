9264e7229dbbd6df3dfba2d649e305a6d8fe23f5
Paulo Santos <paulozullu@gmail.com>
2018-01-09 11:45:35 -0300

    WhiteNoise setup

--- work-at-olist/workatolist/wsgi.py
+++ work-at-olist/workatolist/wsgi.py
@@ -6,6 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
+from whitenoise import WhiteNoise
 
 import os
 
@@ -14,3 +15,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "workatolist.settings")
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='/static/')
